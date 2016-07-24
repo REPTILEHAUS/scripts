02dbd24b79d873fed223a9401a636bc4ed498e2f
Paddy O'Sullivan <padjo.osullivan@gmail.com>
2016-07-24 18:32:32 +0100

    Update userfull-sysadmin-commands.sh

--- userfull-sysadmin-commands.sh
+++ userfull-sysadmin-commands.sh
@@ -5,8 +5,9 @@ tar -tvf backup.tar.gz
 # command and use it in place of /var/www/website/directory-wanted and move it to the /tmp directory
 tar  -zxvf backup.tar.gz -C /tmp   /var/www/website/directory-wanted
 
-
-
 # Rsync using non standard SSH port to move /home/localuser/backup.tar.gz from local server to /home/localuser/backups/backup.tar.gz
 # directory on a remote server while showing the transfer progress on the terminal
 rsync -rvz -e 'ssh -p 3433' --progress ~/backup.tar.gz sysadmin@DestinationServerIP:~/backups/
+
+# Add users to a htpasswd file
+htpasswd -c /etc/apache2/passwords username
