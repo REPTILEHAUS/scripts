e48f20d0d1271df5a4958281ffe102eb6bfd74f2
Paddy O'Sullivan <padjo.osullivan@gmail.com>
2016-07-24 18:30:37 +0100

    A repo of usefull SysAdmin commands
    I plan to regularly add commands I use in here that make the job of managing servers easier.

---
+++ userfull-sysadmin-commands.sh
@@ -0,0 +1,12 @@
+# Show all the folders in a tarball backup
+tar -tvf backup.tar.gz 
+
+# If you wish to extract a single folder/file from the tarball, find its full path in the previous
+# command and use it in place of /var/www/website/directory-wanted and move it to the /tmp directory
+tar  -zxvf backup.tar.gz -C /tmp   /var/www/website/directory-wanted
+
+
+
+# Rsync using non standard SSH port to move /home/localuser/backup.tar.gz from local server to /home/localuser/backups/backup.tar.gz
+# directory on a remote server while showing the transfer progress on the terminal
+rsync -rvz -e 'ssh -p 3433' --progress ~/backup.tar.gz sysadmin@DestinationServerIP:~/backups/
